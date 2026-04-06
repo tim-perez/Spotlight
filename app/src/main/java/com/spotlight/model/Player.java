db7c00efbd2de1240934fbd366fef419022dab55
Tim Perez <timperez624@gmail.com>
2026-04-05 21:23:22 -0700

    Implementing Gradle to run the game

--- app/src/main/java/com/spotlight/model/Player.java
+++ app/src/main/java/com/spotlight/model/Player.java
@@ -5,6 +5,7 @@ import java.io.Serializable;
 public class Player implements Serializable {
     private String name;
     private int score;
+    private String currentAnswer;
 
     public Player(String name) {
         this.name = name;
@@ -21,5 +22,13 @@ public class Player implements Serializable {
 
     public void addScore(int points) {
         this.score += points;
+    }
+
+    public String getCurrentAnswer() {
+        return currentAnswer;
+    }
+
+    public void setCurrentAnswer(String currentAnswer) {
+        this.currentAnswer = currentAnswer;
     }
 }
